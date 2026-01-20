02c180e24d1bdaaf0bf363835bfdab5a157c7c65
PhalePallo <160241510+PhalePallo@users.noreply.github.com>
2026-01-20 11:49:09 +0200

    Add IAM roles for GitHub Actions OIDC integration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,6 @@
+# -------------------------------
+# Lambda IAM Role
+# -------------------------------
 resource "aws_iam_role" "lambda_role" {
   name = "${var.project_name}-lambda-role"
 
@@ -30,4 +33,48 @@ resource "aws_iam_role_policy" "lambda_dynamodb_policy" {
       Resource = aws_dynamodb_table.audit_logs.arn
     }]
   })
+}
+
+# -------------------------------
+# GitHub Actions OIDC Role
+# -------------------------------
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions_terraform" {
+  name = "${var.project_name}-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:PhalePallo/aws-cloud-governance-dashboard:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions_terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # For initial testing
+}
+
+output "github_actions_role_arn" {
+  value       = aws_iam_role.github_actions_terraform.arn
+  description = "IAM role assumed by GitHub Actions via OIDC"
 }
